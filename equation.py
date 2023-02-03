2d3d2c9893c69bdad317421dc8bc9aa51e77944f
Ivan Mikhailov <mikhailov.ivan@gmail.com>
2023-02-03 15:04:37 +0300

    version 03.02

--- equation.py
+++ equation.py
@@ -2,5 +2,14 @@ import numpy as np
 
 def eq(a: float, b: float, c: float):
     """Solution of equation sqrt(a*x+b)=c respect of x"""
-    pass
-
+    if c < 0:
+        raise Exception("there is no solution")
+    elif a == 0 and c != np.sqrt(b):
+        raise Exception("there is no solution")
+    elif a == 0 and c == np.sqrt(b):
+        raise Exception("many solutions")
+    else:
+        return (c**2 - b) / a
+    
+if __name__ == '__main__':
+    print("Hello!")
